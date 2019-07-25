81d6be844d4f8d45b97fc604bd9d9a9af7667827
Andy <andrewwalker@andys-mbp.home>
2019-07-26 00:28:17 +0100

    First update on README

--- R/Testing.R
+++ R/Testing.R
@@ -1,8 +1,10 @@
-library(collidr)
+library(collidr) #One dependancy
+source("R/functions.R")
 
-fname <- "R/Testing.R"
-packagesToInstall <- AutoPackagR(fname)
+fname <- "R/Testing.R" ## Set to current file name
+packagesToInstall <- AutoPackagR(fname) ## Call package installer
 
+### Go call something, including some stuff you don't have installed
 read.alignment("somefile.fasta",format="fasta")
 entropy("ABC")
 phylogeny("some")
